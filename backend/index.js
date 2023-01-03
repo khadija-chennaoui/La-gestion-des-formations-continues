ef62d4e418bd9599e1b0a8691bfd8b7c5a2f10a2
khadija-chennaoui <khadijach896@gmail.com>
2023-01-03 16:54:22 +0100

    ajouter route de emplyer

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,14 @@ const app = express ()
 require('dotenv').config()
 const organismeroute = require('./Routes/organismeRoute')
 const formationroute = require('./Routes/formationRoute')
+const emplyerroute = require('./Routes/emplyerRoute')
 const db = require('./ConfigDB/dataBase')
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use('/organisme',organismeroute)
 app.use('/formation',formationroute)
+app.use('/emplyer',emplyerroute)
 
 
 
