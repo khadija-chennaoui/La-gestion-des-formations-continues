c92750b5b83a8a1a06d84a2dcad8c2583af9292b
khadija-chennaoui <khadijach896@gmail.com>
2023-01-04 15:33:37 +0100

    delet employer et afiche all

--- backend/Controllers/contollerEmplyer.js
+++ backend/Controllers/contollerEmplyer.js
@@ -2,8 +2,10 @@ const Emplyer = require("../Models/users");
 const Role = require("../Models/role");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const storage = require("local-storage");
 
-async function AddEmplyer(req ,res) {
+async function AddEmplyer(req, res) {
   const { body } = req;
   if (!body.name || !body.email || !body.password)
     throw Error("Inputs is not filled");
@@ -24,21 +26,55 @@ async function AddEmplyer(req ,res) {
   }
 }
 
-async function LoginUser(req ,res) {
-const {body} = req
-if(!body.email || !body.password) throw Error('Inputs is not filled')
-else {
-  const existeUser = Emplyer.findOne({email:body.email})
-  if(!existeUser || (await bcrypt.compare(body.password , existeUser.password ))) throw Error('Email or Password inccorect')
-  else{
-    const createToken = await jwt.sign({
-      
-    })
+async function LoginUser(req, res) {
+  const { body } = req;
+  if (!body.email || !body.password) throw Error("Inputs is not filled");
+  else {
+    const existeUser = await Emplyer.findOne({ email: body.email });
+    if (
+      !existeUser ||
+      !(await bcrypt.compare(body.password, existeUser.password))
+    )
+      throw Error("Email or Password inccorect");
+    const createToken = await jwt.sign(
+      { _id: existeUser._id },
+      process.env.SECRET_TOCKEN
+    );
+    storage("token", createToken);
+    const role = await Role.findById(existeUser.role_id);
+    res.json({
+      role_id: role.name,
+      token: storage("token"),
+      name: existeUser.name,
+    });
   }
 }
+
+async function DaletEmplyer(req, res) {
+  const { id } = req.params;
+  console.log(id)
+  try { 
+    const DeletEmployer = await Emplyer.findByIdAndDelete({ _id:id });
+    if (DeletEmployer) res.json({ message: "Emplyer Deleted" });
+  } catch {
+    res.json({ message: "not deleted" });
+  }
+}
+
+async function AllEmployer(req,res){
+  const {body} = req 
+  const allEmployer = await Emplyer.find()
+  try{
+    if(allEmployer) res.json(allEmployer)
+  } catch {
+    throw new Error(error);
+  }
 
 }
 
 module.exports = {
   AddEmplyer,
+  LoginUser,
+  DaletEmplyer,
+  AllEmployer
 };
