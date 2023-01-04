6bf9efb5cb8d3c540026d8acb7c2e4b3ef22eb46
khadija-chennaoui <khadijach896@gmail.com>
2023-01-04 10:19:19 +0100

    Add Emplyer

--- backend/Controllers/contollerEmplyer.js
+++ backend/Controllers/contollerEmplyer.js
@@ -1,20 +1,44 @@
 const Emplyer = require("../Models/users");
-async function AddEmplyer(req, res) {
-    const { body } = req;
-    if(!body.name || !body.email || !body.password ) throw Error("Inputs is not filled");
-    else{
-      const NewEmplyer = await Emplyer.create({
+const Role = require("../Models/role");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+async function AddEmplyer(req ,res) {
+  const { body } = req;
+  if (!body.name || !body.email || !body.password)
+    throw Error("Inputs is not filled");
+  else {
+    const ifEmailExist = await Emplyer.findOne({ email: body.email });
+    if (ifEmailExist) throw Error("emplyer existe déja");
+    else {
+      const hashPassword = await bcrypt.hash(body.password, 10);
+      const emplyerRole = await Role.findOne({ name: "Admin" });
+      const createEmplyer = await Emplyer.create({
         ...body,
+        password: hashPassword,
+        role_id: emplyerRole.id,
       });
-      if (!NewEmplyer) throw Error("eroor");
-      else res.json(NewEmplyer);
+      if (!createEmplyer) throw Error("Error");
+      else res.json({ message: "Emplyer added" });
     }
-  
-  
-   
   }
+}
 
+async function LoginUser(req ,res) {
+const {body} = req
+if(!body.email || !body.password) throw Error('Inputs is not filled')
+else {
+  const existeUser = Emplyer.findOne({email:body.email})
+  if(!existeUser || (await bcrypt.compare(body.password , existeUser.password ))) throw Error('Email or Password inccorect')
+  else{
+    const createToken = await jwt.sign({
+      
+    })
+  }
+}
 
-  module.exports = {
-    AddEmplyer ,
-  };
+}
+
+module.exports = {
+  AddEmplyer,
+};
