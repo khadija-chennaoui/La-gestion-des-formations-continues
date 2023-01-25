7abe115f0347dfc105b1c829c0b7cd70778f3bbd
khadija-chennaoui <khadijach896@gmail.com>
2023-01-25 10:23:44 +0100

    modification route

--- backend/Routes/organismeRoute.js
+++ backend/Routes/organismeRoute.js
@@ -1,6 +1,5 @@
 const route = require('express').Router()
 const organisme = require('../Controllers/controllerOrganisme')
-
 route.post('/add' , organisme.AddOrganisme)
 route.get('/all' , organisme.AllOrganisme)
 route.delete('/delet/:id' , organisme.DeletOrganisme)
